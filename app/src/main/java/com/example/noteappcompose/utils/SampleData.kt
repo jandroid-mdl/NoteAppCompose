6df77806c829716fe2cb5a41a425d05da738c0be
jonathan <jnthnmobileapps@gmail.com>
2025-07-23 19:44:10 -0400

    "Primer commit: estructura inicial del proyecto"

---
+++ app/src/main/java/com/example/noteappcompose/utils/SampleData.kt
@@ -0,0 +1,9 @@
+package com.example.noteappcompose.utils
+
+import com.example.noteappcompose.model.Note
+
+val sampleNotes = listOf(
+    Note(1, "Bañarme", "Antes de tomar once"),
+    Note(2, "Abrigarme", "Evitar enfriarme"),
+    Note(3, "Comer fideos", "Fideos integrales")
+)
